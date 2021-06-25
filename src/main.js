0b845a126cfb061cbc8673e14d0bff2d6184c6d8
White Pai <white_pai@appdet.com>
2021-06-26 00:02:43 +0800

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-// import SignaturePad from 'signature_pad'
-
-// const app = createApp(App);
-// app.use(SignaturePad)
 
 createApp(App).mount('#app')
